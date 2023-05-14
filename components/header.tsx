026008de6de7d73db32fbaeffa8ee2dc5004e639
codingwebsa <codingwebsa@gmail.com>
2023-05-14 17:45:02 +0600

    improved header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "./button";
 import { Container } from "./container";
 import { HamburgerIcon } from "@/icons/hamburger";
@@ -10,6 +10,23 @@ import classNames from "classnames";
 
 export const Header = () => {
   const [hamburgerMenuIsOpen, setHambugerMenuIsOpen] = useState(false);
+
+  useEffect(() => {
+    const html = document.querySelector("html");
+    if (html) html.classList.toggle("overflow-hidden", hamburgerMenuIsOpen);
+  }, [hamburgerMenuIsOpen]);
+
+  useEffect(() => {
+    const closeHamburgerNavigation = () => setHambugerMenuIsOpen(false);
+
+    window.addEventListener("orientationchange", closeHamburgerNavigation);
+    window.addEventListener("resize", closeHamburgerNavigation);
+
+    return () => {
+      window.removeEventListener("orientationchange", closeHamburgerNavigation);
+      window.removeEventListener("resize", closeHamburgerNavigation);
+    };
+  }, [setHambugerMenuIsOpen]);
 
   return (
     <header className="fixed top-0 left-0 w-full border-b border-white-a08 backdrop-blur-[12px]">
@@ -26,8 +43,10 @@ export const Header = () => {
         >
           <nav
             className={classNames(
-              "fixed top-navigation-height left-0 h-[calc(100vh_-_var(--navigation-height))] w-full overflow-auto bg-background transition-opacity duration-500 md:relative md:top-0 md:block md:h-auto md:w-auto md:bg-transparent md:opacity-100",
-              hamburgerMenuIsOpen ? "opacity-100" : "opacity-0"
+              "fixed top-navigation-height left-0 h-[calc(100vh_-_var(--navigation-height))] w-full overflow-auto bg-background transition-opacity duration-500 md:relative md:top-0 md:block md:h-auto md:w-auto md:bg-transparent md:opacity-100 md:translate-x-0",
+              hamburgerMenuIsOpen
+                ? "opacity-100 translate-x-0"
+                : "opacity-0 -translate-x-full"
             )}
           >
             <ul
